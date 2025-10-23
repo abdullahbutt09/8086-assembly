af26aeadaf6d4cc711d602e48f9fd0ae4d2f5f79
Abdullah Butt <abdullah.butt.dev.github@gmail.com>
2025-10-23 16:04:46 +0500

    updating my mul file and adding a div file

---
+++ DIV/div_code.asm
@@ -0,0 +1,7 @@
+; If operand is 8-bit: 
+; AX ÷ operand -> Quotient in AL, Remainder in AH 
+
+; If operand is 16-bit: 
+; DX:AX ÷ operand ? Quotient in AX, Remainder in DX
+
+
--- MUL/mul_code.asm
+++ MUL/mul_code.asm
@@ -8,4 +8,17 @@ MOV AL, 5      ; AL = 5
 MOV BL, 4      ; BL = 4
 MUL BL        ; AL * BL = 5 * 4 = 20
 ; Result ? AX = 0014h
-; AL = 14h (20), AH = 00h                  
+; AL = 14h (20), AH = 00h    
+                     
+MOV AL, 50     ; AL = 50
+MOV BL, 10     ; BL = 10
+MUL BL         ; 50 * 10 = 500 (01F4h)
+; Result ? AX = 01F4h
+; AH = 01h (upper byte), AL = F4h (lower byte)
+
+MOV AX, 1000   ; AX = 1000
+MOV BX, 10     ; BX = 10
+MUL BX         ; 1000 * 10 = 10000 (2710h)
+; Result ? DX:AX = 0000:2710
+                    
+                           
